e6b47c2de1ed553a1a5c07f25751b2aa311064bd
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-10-07 23:30:18 -0700

    Reset back to initial values

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import { ViteRsw } from 'vite-plugin-rsw';
 
 export default defineConfig({
-  plugins: [solid(), ViteRsw(),],
+  plugins: [solid()],
 })
