f43aba87e17e87f882a808042f055343dddc76f7
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-10-08 15:05:05 -0700

    First attempt to write custom signalling server

---
+++ signalserver/src/main.rs
@@ -0,0 +1,72 @@
+use std::{collections::HashMap, net::SocketAddr};
+
+use async_trait::async_trait;
+use axum::{extract::ws::Message, Error};
+use matchbox_protocol::PeerId;
+use matchbox_signaling::{
+    NoCallbacks, SignalingServerBuilder, SignalingState, SignalingTopology, WsStateMeta,
+};
+use tokio::sync::mpsc::UnboundedSender;
+use tracing::info;
+use tracing_subscriber::prelude::*;
+use uuid::Uuid;
+
+fn setup_logging() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "signalserver=info,tower_http=debug".into()),
+        )
+        .with(
+            tracing_subscriber::fmt::layer()
+                .compact()
+                .with_file(false)
+                .with_target(false),
+        )
+        .init();
+}
+
+#[derive(Default, Debug, Clone)]
+struct ServerState {
+    rooms: HashMap<Uuid, Room>,
+}
+impl SignalingState for ServerState {}
+
+#[derive(Debug, Clone)]
+struct Room {
+    peers: Vec<Peer>,
+}
+
+#[derive(Debug, Clone)]
+struct Peer {
+    uuid: PeerId,
+    sender: UnboundedSender<Result<Message, Error>>
+}
+
+#[tokio::main]
+async fn main() {
+    setup_logging();
+
+    info!("Initialising Signal Server");
+
+    let server_state = ServerState::default();
+
+    let addr: SocketAddr = "0.0.0.0:3536".parse().unwrap();
+    let server = SignalingServerBuilder::new(addr, ChatRoomTopology, server_state);
+}
+
+struct ChatRoomTopology;
+
+#[async_trait]
+impl SignalingTopology<NoCallbacks, ServerState> for ChatRoomTopology {
+    async fn state_machine(upgrade: WsStateMeta<NoCallbacks, ServerState>) {
+        let WsStateMeta { peer_id, sender, mut receiver, mut state, .. } = upgrade;
+
+        let connecting_peer = Peer { uuid: peer_id, sender };
+
+        let room
+
+
+
+    }
+}
