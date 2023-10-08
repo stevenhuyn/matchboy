2f37dda47c1622fab95e6abf27ccde1df0cc8161
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-10-08 15:48:56 -0700

    Handle dumb connection

--- signalserver/src/main.rs
+++ signalserver/src/main.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashMap, net::SocketAddr};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
 use async_trait::async_trait;
 use axum::{extract::ws::Message, Error};
-use matchbox_protocol::PeerId;
+use matchbox_protocol::{JsonPeerEvent, PeerId};
 use matchbox_signaling::{
     common_logic::SignalingChannel, NoCallbacks, SignalingServerBuilder, SignalingState,
     SignalingTopology, WsStateMeta,
@@ -29,14 +33,14 @@ fn setup_logging() {
 
 #[derive(Default, Debug, Clone)]
 struct ServerState {
-    rooms: HashMap<Uuid, Room>,
-    next: Option<NextPeer>,
+    rooms: Arc<Mutex<HashMap<Uuid, Room>>>,
+    next: Arc<Mutex<Option<NextPeer>>>,
 }
 impl SignalingState for ServerState {}
 
 #[derive(Debug, Clone)]
 struct NextPeer {
-    room_id: Uuid,
+    pub room_id: Uuid,
 }
 
 #[derive(Debug, Clone)]
@@ -56,22 +60,39 @@ async fn main() {
 
     info!("Initialising Signal Server");
 
-    let server_state = ServerState::default();
+    let mut state = ServerState::default();
+    let mut request_state = state.clone();
+
+    let uuid = Uuid::parse_str("c957a42c-ec98-41fd-be84-4cd7f4a584fd").unwrap();
+
+    state
+        .rooms
+        .lock()
+        .unwrap()
+        .insert(uuid, Room { peers: Vec::new() });
 
     let addr: SocketAddr = "0.0.0.0:3536".parse().unwrap();
-    let server = SignalingServerBuilder::new(addr, ChatRoomTopology, server_state)
+    let server = SignalingServerBuilder::new(addr, ChatRoomTopology, state)
         .on_connection_request(move |connection| {
-            info!("Connection Request {connection:?}");
+            // info!("Connection Request {connection:?}");
             Ok(true)
         })
         .on_id_assignment(move |(origin, peer_id)| {
-            info!("Client connected {origin:?}: {peer_id:?}");
+            request_state
+                .next
+                .lock()
+                .unwrap()
+                .replace(NextPeer { room_id: uuid });
+            // info!("Client connected {origin:?}: {peer_id:?}");
         })
         .cors()
         .trace()
         .build();
 
-        server.serve().await.expect("Unable to run signalling server");
+    server
+        .serve()
+        .await
+        .expect("Unable to run signalling server");
 }
 
 struct ChatRoomTopology;
@@ -87,11 +108,29 @@ impl SignalingTopology<NoCallbacks, ServerState> for ChatRoomTopology {
             ..
         } = upgrade;
 
+        info!("Upgrade - {state:?}");
+
         let connecting_peer = Peer {
             uuid: peer_id,
             sender,
         };
 
-        // for peer in state.rooms
+        let room_id = state.next.lock().unwrap().as_ref().unwrap().room_id;
+
+        info!("Room ID - {room_id:?}");
+        let event_text = JsonPeerEvent::NewPeer(peer_id).to_string();
+        let event = Message::Text(event_text.clone());
+
+        {
+            let rooms = state.rooms.lock().unwrap();
+            let room = rooms.get(&room_id).unwrap();
+            for peer in room.peers.iter() {
+                peer.sender.send(Ok(event.clone())).unwrap();
+            }
+        }
+
+        let mut rooms = state.rooms.lock().unwrap();
+        let room = rooms.get_mut(&room_id).unwrap();
+        room.peers.push(connecting_peer);
     }
 }
