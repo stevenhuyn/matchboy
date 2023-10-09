bd3a7ccf7d521e2a31a8e4fbc6bc85f41e082468
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-10-08 23:37:43 -0700

    Swap order of typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: ["retro"],
   },
